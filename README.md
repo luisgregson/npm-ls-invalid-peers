69e7bece724c48f34cae06d66d933ba3ac4e5d20
Luis Gregson <luis.gregson@korelogic.co.uk>
2023-04-13 14:18:12 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,48 @@
+# npm-ls-invalid-peers
+
+`npm-ls-invalid-peers` is a command-line tool that analyzes the output of `npm ls --all` to identify packages with invalid peer dependency versions, grouped by the package on which they depend. This tool helps you easily find and fix invalid peer dependencies in your project.
+
+## Usage
+
+There are two ways to use `npm-ls-invalid-peers`:
+
+### 1. Installing globally and running from a directory
+
+Install the package globally:
+
+```sh
+npm install -g npm-ls-invalid-peers
+```
+
+Then, run the command from any directory:
+
+```sh
+npm-ls-invalid-peers
+```
+
+### 2. Running with npx
+
+Without installing the package, run the following command from any directory:
+
+```sh
+npx npm-ls-invalid-peers
+```
+
+## Example
+
+After running the `npm-ls-invalid-peers` command in a project, you may see output similar to the following:
+
+```
+react@17.0.2
+  -  node_modules/@blueprintjs/datetime requires: "^15.3.0 || 16" 
+  -  node_modules/@blueprintjs/select requires: ^15.3.0 || 16"
+  -  ...
+react-dom@17.0.2
+  - node_modules/@blueprintjs/datetime: "^15.3.0 || 16" 
+  - ...
+```
+This output shows the packages with invalid peer dependencies, grouped by the packages on which they depend.
+
+## License
+
+This project is licensed under the MIT License. See the [LICENSE](LICENSE) file for details.
---
+++ src/index.js
@@ -0,0 +1,49 @@
+const { exec } = require('child_process');
+
+function lsInvalidPeers() {
+  exec('npm ls --all', (err, data) => {
+    if (err) {
+      console.error('Error running npm ls --all:', err);
+      return;
+    }
+
+
+    const lines = data.split('\n');
+    const dedupedLines = lines.filter(line => line.includes('deduped invalid') || line.includes('deduped'));
+
+    const dependencies = {};
+
+    for (const line of dedupedLines) {
+      const match = line.match(/(react(?:-dom)?@\d+\.\d+\.\d+)/);
+
+      if (!match) {
+        continue;
+      }
+
+      const dependency = match[1];
+
+      if (!dependencies[dependency]) {
+        dependencies[dependency] = [];
+      }
+
+      const packageMatch = line.match(/node_modules\/([^/]+)/);
+      const requiredVersionMatch = line.match(/invalid: "([^"]+)"/);
+
+      if (packageMatch && requiredVersionMatch) {
+        const packageName = packageMatch[1];
+        const requiredVersion = requiredVersionMatch[1];
+        dependencies[dependency].push(`${packageName}: "${requiredVersion}"`);
+      }
+    }
+
+    for (const [dependency, packages] of Object.entries(dependencies)) {
+      console.log(`${dependency}`);
+      for (const packageInfo of packages) {
+        console.log(`  - ${packageInfo}`);
+      }
+      console.log('');
+    }
+  });
+}
+
+module.exports = lsInvalidPeers;
